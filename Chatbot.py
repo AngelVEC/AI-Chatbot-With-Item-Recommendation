09f46f1c798728f7d844ee4ee7012283d539b81b
AngelVOC <angelmystic15@gmail.com>
2026-04-14 15:29:17 +1200

    added safe clause, when there is no keywords matches.

--- Chatbot.py
+++ Chatbot.py
@@ -6,8 +6,12 @@ class Chatbot:
     def handle_query(self, user_query):
         intent = self.gemini_service.extract_intent(user_query)
 
-        print(intent)
         results = self.database.search_products(intent)
+
+        if not results:
+            fallback_intent = intent.copy()
+            fallback_intent["keywords"] = []
+            results = self.database.search_products(fallback_intent)
 
         if results:
             return self.gemini_service.recommend(user_query, results)
