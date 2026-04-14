db837d8fca023b6e8b40909100df91ac0181bdf5
AngelVOC <angelmystic15@gmail.com>
2026-04-14 17:09:39 +1200

    added temporary conversation memory to the chatbot (reset between runs) to test if it improves the chatbot's ability to maintain context across multiple interactions. This is a simple implementation and can be further enhanced with a more robust memory management system in the future.

--- Chatbot.py
+++ Chatbot.py
@@ -2,6 +2,7 @@ class Chatbot:
     def __init__(self, gemini_service, database):
         self.gemini_service = gemini_service
         self.database = database
+        self.memory = []
     
     def handle_query(self, user_query):
         intent = self.gemini_service.extract_intent(user_query)
@@ -14,6 +15,12 @@ class Chatbot:
             results = self.database.search_products(fallback_intent)
 
         if results:
-            return self.gemini_service.recommend(user_query, results)
-
-        return "I couldn't find any products matching your query. try different keywords or check back later when we have more products in our database!"
+            response = self.gemini_service.recommend(user_query, results, memory=self.memory)
+        else:
+            response = self.gemini_service.generate_response("No products found. Help the user to choose based on the recent conversation", memory=self.memory)
+        
+        #storing the memory of the conversation
+        self.memory.append({"user": user_query, "bot": response})
+        self.memory = self.memory[-5:]  # Keep only the last 5 conversations in memory
+        
+        return response
